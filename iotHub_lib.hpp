300c47c96a8b6b0b772c476853b4bfed5621bfe0
Nixes <callum@see-bry.com>
2016-08-18 22:36:06 +1000

    switch to using httpclient

--- iotHub_lib.hpp
+++ iotHub_lib.hpp
@@ -1,5 +1,6 @@
 #include "ArduinoJson.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 template<uint array_size> class iotHubLib {
 private:
@@ -7,18 +8,10 @@ private:
   int iothub_port= 80;
   bool sensor_configs_loaded = false;
 
-  WiFiClient client;
 
   int tick_time = 10000; // default of 10 seconds
   char sensor_ids[array_size][25]; // array of sensor ID's, sensor ids are 25 alphanumeric keys long
   //char* sensor_ids[][25]; // array of sensor ID's, sensor ids are 25 alphanumeric keys long
-
-  void Connect() {
-    Serial.print("Attempting to connect to server\n");
-    if (client.connect(iothub_server, 80) ) {
-      Serial.println("connected to server");
-    }
-  };
 
   // this should read sensor ID's from internal memory if available, else ask for new ids from the given server
   void LoadSensors() {
@@ -30,24 +23,29 @@ private:
 
   void RegisterSensor(char* sensor_name, char sensor_id[25] ) {
     Serial.println("Registering sensor");
+    HTTPClient http;
+
     // Make a HTTP post
-    client.println("POST /api/sensors");
+    http.begin(iothub_server,iothub_port,"/api/sensors");
 
     // prep the json object
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& json_obj = jsonBuffer.createObject();
     json_obj["name"] = sensor_name;
 
-    client.println(" HTTP/1.1");
-    client.print("Host: "); client.println(iothub_server);
-    client.println("Content-Type: application/json"); // important! JSON conversion in nodejs requires this
+    http.addHeader("Content-Type","application/json"); // important! JSON conversion in nodejs requires this
 
-    // send length of the json to come
-    client.print("Content-Length: "); client.println(json_obj.measureLength());
-    client.println();
+    // add the json to a string
+    String json_string;
+    json_obj.printTo(json_string);// this is great except it seems to be adding quotation marks around what it is sending
+    // then send the json
+    http.POST(json_string);
 
-    // send the json
-    json_obj.printTo(client);// this is great except it seems to be adding quotation marks around what it is sending
+    // then print the response over Serial
+    Serial.print("Response: ");
+    Serial.println( http.getString() );
+
+    http.end();
   }
 
 public:
@@ -56,7 +54,6 @@ public:
     iothub_server = server;
     iothub_port = port;
     Serial.begin(115200);
-    Connect();
     LoadSensors();
   };
   // destructor
@@ -66,24 +63,29 @@ public:
   void StartConfig() {};
 
   void Send(uint sensor_index,float sensor_value) {
+      HTTPClient http;
+
       // Make a HTTP post
-      client.print("POST /api/sensors/"); client.print(sensor_ids[sensor_index]); client.println("/data");
+      http.begin(iothub_server,iothub_port,"/api/sensors/" + sensor_ids[sensor_index] + "/data");
 
       // prep the json object
       StaticJsonBuffer<50> jsonBuffer;
       JsonObject& json_obj = jsonBuffer.createObject();
       json_obj["value"] = sensor_value;
 
-      client.println(" HTTP/1.1");
-      client.print("Host: "); client.println(iothub_server);
-      client.println("Content-Type: application/json"); // important! JSON conversion in nodejs requires this
+      http.addHeader("Content-Type", "application/json"); // important! JSON conversion in nodejs requires this
 
-      // send length of the json to come
-      client.print("Content-Length: "); client.println(json_obj.measureLength());
-      client.println();
+      // add the json to a string
+      String json_string;
+      json_obj.printTo(json_string);// this is great except it seems to be adding quotation marks around what it is sending
+      // then send the json
+      http.POST(json_string);
 
-      // send the json
-      json_obj.printTo(client);// this is great except it seems to be adding quotation marks around what it is sending
+      // then print the response over Serial
+      Serial.print("Response: ");
+      Serial.println( http.getString() );
+
+      http.end();
   };
 
   // this should post to /api/sensors with the requested sensor, this will then return an ID that can be used
