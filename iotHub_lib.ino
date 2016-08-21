3346099c808221425d8156d5fb692e361b9590f9
Nixes <callum@see-bry.com>
2016-08-21 22:21:06 +1000

    update example for latest lib version

--- iotHub_lib.ino
+++ iotHub_lib.ino
@@ -1,18 +1,24 @@
 #include "iotHub_lib.hpp"
+#include "DHT.h" // using adafruit DHT lib: https://github.com/adafruit/DHT-sensor-library
 
+// init iothublib, number in <> specifies number of sensors being used on this node
+// the two arguments are the hostname of the server and the port iothub is available on
+iotHubLib<2> iothub("callum-desktop",3000); // note lack of http:// prefix, do not add one
 
-iotHubLib<2> iothub("callum-desktop",3000);
-
+// init the temp sensors
+DHT dht_0(5, DHT22);
+DHT dht_1(4, DHT22);
 
 void setup() {
   iothub.Start();
+  
   // add sensors
   char *names[] = {"Temperature Sensor 1","Temperature Sensor 2"};
   iothub.RegisterSensors(names);
 }
 
 void loop() {
-  iothub.Send(1,11.12)
-  iothub.Send(0,10.1);
+  iothub.Send(0, dht_0.readTemperature() );
+  iothub.Send(1, dht_1.readTemperature() );
   iothub.Tick();
 }
