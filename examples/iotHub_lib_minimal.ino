3b259cefd9e8f6fbb30b2d6e464e542033046bc9
Nixes <callum@see-bry.com>
2016-12-12 18:30:17 +1100

    make arduino library spec compatible, fixes #9

---
+++ examples/iotHub_lib_minimal.ino
@@ -0,0 +1,16 @@
+#include "iotHub_lib.hpp"
+
+// to init iothublib use syntax like <sensors,actors>,
+// where sensors specifies number of sensors being used on this node
+// and actors specifies the number of actors being used on this node
+// the two arguments in the () are the hostname of the server and the port iothub is available on
+iotHubLib<2,0> iothub("linserver",3000); // note lack of http:// prefix, do not add one
+
+void setup() {
+  iothub.Start();
+
+}
+
+void loop() {
+  iothub.Tick();
+}
