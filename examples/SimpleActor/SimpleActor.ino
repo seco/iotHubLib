154c79ad232c9396cc2dccbf0a32bd30ca4ec31a
Nixes <callum@see-bry.com>
2017-01-01 15:41:59 +1100

    correct actor example

--- examples/SimpleActor/SimpleActor.ino
+++ examples/SimpleActor/SimpleActor.ino
@@ -4,7 +4,7 @@
 // where sensors specifies number of sensors being used on this node
 // and actors specifies the number of actors being used on this node
 // the two arguments in the () are the hostname of the server and the port iothub is available on
-iotHubLib<2,0> iothub("linserver",3000); // note lack of http:// prefix, do not add one
+iotHubLib<0,1> iothub("linserver",3000); // note lack of http:// prefix, do not add one
 
 void actor1_callback(int actor_state) {
   Serial.print("Recieved actor_state: ");
